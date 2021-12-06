f2af7c458738171b9a1572489f224c6cc89c45fb
atthana-p <atthana.p@codium.co>
2021-12-06 22:37:46 +0700

    4. makemigration, migrate db + เพิ่มสิ่งที่จะให้มองเห็นใน django admin ใน admin.py + allow host ใน settings.py

--- covid_self_monitoring/covid_self_monitoring/settings.py
+++ covid_self_monitoring/covid_self_monitoring/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^6x8&14w)ql_6v_do^=(gpuzw(8sq!x(yo%6tq1ci=(1z5sci3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'monitor',
 ]
 
 MIDDLEWARE = [
--- covid_self_monitoring/monitor/admin.py
+++ covid_self_monitoring/monitor/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+# Register ทั้ง 2 models นะ
+@admin.register(models.Symptom)
+class SymptomAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created', 'temperature',
+                    'o2sat', 'systolic', 'diastolic', 'symptoms_display']
+    # symptoms_diaplay มันคือ ตัวที่อยู่ใน @property นั่นแหละใน models.py ที่ได้เราสร้างไว้ก่อนหน้านี้
+    # ชื่อของ fileds ต่างๆที่จะเอามาโชว์ จะต้องพิมพ์ให้ถูกด้วยนะ ต้องเป๊ะ
+    # พวก list พวกนี้มันมีใน Django หมดแล้ว กำหนดได้ว่าจะให้โชว์อะไรบ้าง
+    list_filter = ['user', 'created']
+    # พวกนี้คือเราต้องการให้ filter จากด้านข้างได้ด้วย โดยให้ filter ได้จาก 2 fields นะ
+    filter_horizontal = ['symptoms']
+    # filter_horizontal ทำให้มัน filter ได้ง่ายขึ้น
