e6f4830d7921c5ff4e8c48a090d1e7497dd31b68
atthana-p <atthana.p@codium.co>
2021-12-06 23:37:34 +0700

    T003: อันนี้จะแก้ไขจากการ return ออกมาเป็น HttpResponse ให้แปลงเป็น json แทน

--- covid_self_monitoring/monitor/views.py
+++ covid_self_monitoring/monitor/views.py
@@ -1,7 +1,11 @@
+import json
+
 from django.http import HttpResponse
 
 from . import models
 
 
 def current_temperature(request):
-    return HttpResponse(f'Temperature: {models.Measurement.objects.last().temperature}')  # เป็นการดึงค่า temperature ที่เป็น last() ออกมาจาก db นะ
+    data = {'temperature': str(models.Measurement.objects.last().temperature)}  # อันนี้เพราะ temp มันเป็น decimal field นะ จึงต้องแปลงให้เป็น String ก่อน
+    return HttpResponse(json.dumps(data))
+
