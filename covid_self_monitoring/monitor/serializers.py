ccaf6804438637ee3ce403818bf182b4f4bd866b
atthana-p <atthana.p@codium.co>
2021-12-08 00:56:54 +0700

    T008: การทำ Nested serializer.

--- covid_self_monitoring/monitor/serializers.py
+++ covid_self_monitoring/monitor/serializers.py
@@ -2,16 +2,25 @@ from django.contrib.auth import get_user_model
 from django.utils import timezone
 from rest_framework import serializers
 
-from .models import Measurement
+from .models import Measurement, Symptom
 
 User = get_user_model()
 
 
+class SymptomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Symptom
+        fields = '__all__'
+
+
 class MeasurementSerializer(serializers.ModelSerializer):
     created = serializers.DateTimeField(default=timezone.now())
+    symptoms = SymptomSerializer(many=True)
+    # symptoms ตรงนี้เราจะ serialize มันออกไปอย่างไร ก็เอามาจาก SymptomSerializer ไง
+    # เป็นการ serialize ตัว many to many ออกมา แล้วได้ทั้ง key และ value ออกมา
 
     class Meta:
         model = Measurement
-        fields = ['id', 'created', 'temperature', 'o2sat', 'systolic', 'diastolic', 'user']
+        fields = '__all__'
 
 
