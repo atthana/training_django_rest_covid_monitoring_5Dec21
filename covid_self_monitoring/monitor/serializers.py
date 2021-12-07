9075360f868582a370baaf0312dedd2505f7381b
atthana-p <atthana.p@codium.co>
2021-12-07 20:56:37 +0700

    T007: ทดลองเพิ่มค่าเข้า db โดยวิธีการ แปลงจาก dict ให้เป็น object ผ่านทาง Serializer ซึ่งจะต้อง implement create() ขึ้นมาด้วยนะ เพราะเป็นการสร้างข้อมูลใหม่

--- covid_self_monitoring/monitor/serializers.py
+++ covid_self_monitoring/monitor/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.utils import timezone
 from rest_framework import serializers
 
+from .models import Measurement
+
 User = get_user_model()
 
 
@@ -13,3 +15,8 @@ class MeasurementSerializer(serializers.Serializer):
     systolic = serializers.IntegerField()
     diastolic = serializers.IntegerField()
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    def create(self, validated_data):
+        # data = {'temperature': 36.5, 'o2sat': 98, 'systolic': 120, 'diastolic': 79, 'user': User.objects.last().id}
+        # **data = 'temperature': 36.5, 'o2sat': 98, 'systolic': 120, 'diastolic': 79, 'user': User.objects.last().id
+        return Measurement.objects.create(**validated_data)
