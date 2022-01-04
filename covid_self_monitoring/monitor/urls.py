9f7946b97db323b9592ec15927263b92d67ef78d
atthana-p <atthana.p@codium.co>
2022-01-05 00:39:35 +0700

    T010: 2:53:08 ทดลองสร้าง Response จาก Class based views บ้าง (จากตอนแรกที่ใช้ Function based views ที่มี @api_view อ่ะนะ).

--- covid_self_monitoring/monitor/urls.py
+++ covid_self_monitoring/monitor/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('current-temperature/', views.current_temperature),
-    path('all-measurement/', views.all_measurement)
+    path('all-measurement/', views.all_measurement),
+    path('all-measurement-api-view', views.AllMeasurementView.as_view())
+    # ถ้าเป็น class จะต้องสั่ง as_view() ด้วยนะ สำคัญนะ, ไม่เหมือน function view ที่ใส่ไปเฉยๆนะ
 ]
--- covid_self_monitoring/monitor/views.py
+++ covid_self_monitoring/monitor/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from . import models
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view  # ตัวนี้เป็น decorator ใช้สำหรับ Function based views
+from rest_framework.views import APIView  # For Class based views.
+
 from .models import Measurement
 from .serializers import MeasurementSerializer
 
@@ -11,10 +13,17 @@ from .serializers import MeasurementSerializer
 def current_temperature(request):
     data = {'temperature': str(
         models.Measurement.objects.last().temperature)}
-    return HttpResponse(json.dumps(data))
+    return HttpResponse(json.dumps(data))  # อันนี้คือ response ตัวแรกของ Django เลยแบบ Simple ที่สุด
 
 
 @api_view(['GET'])
 def all_measurement(request):
     serializer = MeasurementSerializer(Measurement.objects.all(), many=True)
-    return Response(data=serializer.data)
+    return Response(data=serializer.data)  # ถัดไป response แบบนี้จะใช้กับ Function Based Views
+
+
+# หลังจากนี้ไป เราจะใช้ Response ของ rest_framework ทั้งหมดนะ
+class AllMeasurementView(APIView):
+    def get(self, request):  # APIView ข้างบน มันจะมี get นะ
+        serializer = MeasurementSerializer(Measurement.objects.all(), many=True)
+        return Response(data=serializer.data)  # พวกนี้จะ return เหมือนกับ api_view ข้างบนเลยนะ
