5176926fc444ae5f5857d8b22934758ce366981a
atthana-p <atthana.p@codium.co>
2022-01-06 01:26:12 +0700

    4:15:15 ทดสอบใช้ generics.ListAPIView

--- covid_self_monitoring/monitor/urls.py
+++ covid_self_monitoring/monitor/urls.py
@@ -6,6 +6,9 @@ from . import views
 urlpatterns = [
     path('current-temperature/', views.current_temperature),
     path('all-measurement/', views.all_measurement),
-    path('all-measurement-api-view', views.AllMeasurementView.as_view())
+    path('all-measurement-api-view', views.AllMeasurementView.as_view()),
     # ถ้าเป็น class จะต้องสั่ง as_view() ด้วยนะ สำคัญนะ, ไม่เหมือน function view ที่ใส่ไปเฉยๆนะ
+
+    path('measurement-generics-view/', views.MeasurementGenericsView.as_view())
+    # ต้องมี as_view() ด้วยนะ อย่าลืม เพราะมันเป็น class view.
 ]
--- covid_self_monitoring/monitor/views.py
+++ covid_self_monitoring/monitor/views.py
@@ -1,11 +1,12 @@
 import json
-from django.http import HttpResponse
-from . import models
 
-from rest_framework.response import Response
+from django.http import HttpResponse
+from rest_framework import generics
 from rest_framework.decorators import api_view  # ตัวนี้เป็น decorator ใช้สำหรับ Function based views
+from rest_framework.response import Response
 from rest_framework.views import APIView  # For Class based views.
 
+from . import models
 from .models import Measurement
 from .serializers import MeasurementSerializer
 
@@ -25,5 +26,11 @@ def all_measurement(request):
 # หลังจากนี้ไป เราจะใช้ Response ของ rest_framework ทั้งหมดนะ
 class AllMeasurementView(APIView):
     def get(self, request):  # APIView ข้างบน มันจะมี get นะ
-        serializer = MeasurementSerializer(Measurement.objects.all(), many=True)
+        serializer = MeasurementSerializer(Measurement.objects.all(), many=True)  # query ค่า แล้วก้อ serializer ออกไป
         return Response(data=serializer.data)  # พวกนี้จะ return เหมือนกับ api_view ข้างบนเลยนะ
+
+
+class MeasurementGenericsView(generics.ListAPIView):  # อันนี้มันคือการ GET ออกมานะ
+    # อย่างที่บอกนะ property ที่มันต้องการ 2 ตัวคือ queryset กับ serializer_class
+    queryset = Measurement.objects.all()  # queryset เพื่อบอกให้รู้ว่าตอนที่มันจะไปสร้าง serializer เนี่ยมันต้อง query อะไรออกมาบ้าง
+    serializer_class = MeasurementSerializer  # ตอนที่มันจะแปลงค่าจาก object ให้กลายเป็น json จะต้องใช้ serializer ตัวไหน
