9595470bc39b232b2c14256babfdd0523d508d8f
atthana-p <atthana.p@codium.co>
2022-01-10 02:51:05 +0700

    T021 6:54:06 สร้าง API Doc ของ Swagger แบบที่โบนัสทำนะ

--- covid_self_monitoring/covid_self_monitoring/settings.py
+++ covid_self_monitoring/covid_self_monitoring/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'django_extensions',
+    'drf_yasg',
+
     'monitor',
 ]
 
